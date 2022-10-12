76f8daa59cbf0458009c2ac5b698ccc72a5faa44
Ypisdss <114306975+Ypisdss@users.noreply.github.com>
2022-10-12 15:30:45 -0300

    Update CodigoThiago.c

--- CodigoThiago.c
+++ CodigoThiago.c
@@ -35,7 +35,6 @@ typedef struct{
 void DesenharPersonagem(Texture2D personagem, Personagem perso, int andando, int posicao) {
     DrawTextureRec(personagem, perso.rec, perso.posicao, RAYWHITE);
 }
-
 void carregarRetangulos(Rectangle *rec_map,int screenWidth, int screenHeight, Texture2D mapa) {
     
     rec_map[0].x = (screenWidth / 2 - mapa.width /2) + 17;
@@ -66,11 +65,7 @@ void carregarRetangulos(Rectangle *rec_map,int screenWidth, int screenHeight, Te
     rec_map[5].x = (screenWidth / 2 - mapa.width /2) + 80;
     rec_map[5].y = (screenHeight /2 - mapa.height / 2) + 150;
     rec_map[5].width = 12;
-    rec_map[5].height = 53;
-
-
-    
-    
+    rec_map[5].height = 53;  
 }
 
 void verificaColisao(int *andando, Rectangle *rec_map, Personagem perso, int posicao) {
@@ -95,6 +90,102 @@ void verificaColisao(int *andando, Rectangle *rec_map, Personagem perso, int pos
     }
 }
 
+void initCamera(Camera2D *camera) {
+    camera -> offset.x = 0;
+    camera -> offset.y = 0; 
+    camera -> rotation = 0.0f;
+    camera -> zoom = 1.5;
+}
+
+void updateCamera(Camera2D *camera, Personagem *perso, int screenWidth, int screenHeight, Texture2D mapa) {
+    if(perso -> posicao.x -(screenWidth /2) /1.5 <= (screenWidth /2 - mapa.width /2)) {
+        camera -> target.x = (screenWidth /2 - mapa.width /2);
+                     
+    }
+    else if(perso -> posicao.x -(screenWidth /2) /1.5 >= mapa.width /2 - ((screenWidth / 2) / 1.5) / 2 ) {
+        camera -> target.x = mapa.width /2 - ((screenWidth / 2) / 1.5) /2 ;
+                
+    }
+    else {
+        camera -> target.x = perso -> posicao.x - (screenWidth /2) /1.5;
+    }
+            
+    if(perso -> posicao.y - (screenHeight / 2) / 1.5 <=(screenHeight /2 - mapa.height /2) + ((screenHeight / 2) / 1.5) / 8 ) {
+        camera -> target.y = (screenHeight /2 - mapa.height /2) + ((screenHeight / 2) / 1.5) / 8;
+    }
+    else if(perso -> posicao.y - (screenHeight / 2) / 1.5 >= mapa.height /2 - ((screenHeight / 2) / 1.5) / 2) {
+        camera -> target.y = mapa.height /2 - ((screenHeight / 2) / 1.5) / 2;
+    }
+    else {
+        camera -> target.y = perso -> posicao.y - (screenHeight /2) / 1.5;
+    }
+}
+
+void initPerso(Personagem *perso, float frameWidth, float frameHeight) {
+    perso -> rec.height = frameHeight;
+    perso -> rec.width = frameWidth;
+    perso -> hitbox.width = frameWidth / 2;
+    perso -> hitbox.height = frameHeight / 2 + 10;
+}
+
+void updatePerso(Personagem *perso, int *x, int *y, int *frame, float *time, int frameWidth, int frameHeight, int *andando, int *posicao, int screenWidth, int screenHeight, Rectangle *rec_map) {
+        perso -> rec.x = (*frame) * frameWidth;
+        perso -> rec.y = (*posicao) * frameHeight;
+        perso -> posicao.x = screenWidth / 2 + *x;
+        perso -> posicao.y = screenHeight /2 + *y;
+        perso -> hitbox.x = (screenWidth / 2 + frameWidth /4) + *x;
+        perso -> hitbox.y = (screenHeight / 2 + frameHeight / 4) + *y;
+
+        int yaux = *y;
+        int xaux = *x;
+
+        if(IsKeyDown(KEY_A) == true) {
+            xaux = xaux - 2;
+            *andando = 1;
+            *posicao = 1;
+            verificaColisao(andando, rec_map, *perso, *posicao);
+            if(*andando == 1) *x = xaux;
+        }
+        else if(IsKeyDown(KEY_D) == true) {
+            xaux = xaux + 2;
+            *andando = 1;
+            *posicao = 2;
+            verificaColisao(andando, rec_map, *perso, *posicao);
+            if(*andando == 1) *x = xaux;
+        }
+        else if(IsKeyDown(KEY_W) == true) {
+            yaux = yaux -2;
+            *andando = 1;
+            *posicao = 3;
+            verificaColisao(andando, rec_map, *perso, *posicao);
+            if(*andando == 1) *y = yaux;
+
+        }
+        else if(IsKeyDown(KEY_S) == true) {
+            yaux = yaux + 2;
+            *andando = 1;
+            *posicao = 0;
+            verificaColisao(andando, rec_map, *perso, *posicao);
+            if(*andando == 1) *y = yaux;
+        }
+        else {
+            *andando = 0;
+            *frame = 0;
+        }
+
+        if(*andando == 1) {
+            *time += GetFrameTime();
+            if(*time >= 0.2f) {
+                (*frame)++;
+                *time = 0.0f;
+            }
+            *frame = *frame % 4;
+        }
+}
+
+
+
+
 
 
 //------------------------------------------------------------------------------------
@@ -104,32 +195,20 @@ int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 1000;
-    const int screenHeight = 900;
+    const int screenWidth = 1200;
+    const int screenHeight = 600;
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
     // Textures:
-    int x = 0, y = 0, andando = 0, posicao = 0, xaux, yaux;
-
+    int x = 0, y = 0, andando = 0, posicao = 0;
     Personagem perso;
-    perso.posicao.x = screenWidth / 2;
-    perso.posicao.y = screenHeight / 2;
-
+    
     Camera2D camera;
-    camera.offset.x = 0;
-    camera.offset.y = 0;
-    camera.target.x = perso.posicao.x;
-    camera.target.y = perso.posicao.y;
-    
-    camera.rotation = 0.0f;
-    camera.zoom = 1.5;
-
-    
-    
-    
+    initCamera(&camera);
+     
     Texture2D mapa = LoadTexture("./Assets/MapaDefinitivo.png");
     Texture2D personagem = LoadTexture("./Assets/perso.png");
     float frameWidth = personagem.width / 4.0;
@@ -137,23 +216,11 @@ int main(void)
     int frame = 0;
     float time = 0.0f;
 
-    
-    
+    initPerso(&perso, frameWidth, frameHeight);
 
     Rectangle *rec_map = (Rectangle *) malloc(10 * sizeof(Rectangle));
     carregarRetangulos(rec_map, screenWidth, screenHeight, mapa);
     
-    
-
-   
-    
-    
-
-    
-    
-    
-
-   
     
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
@@ -162,75 +229,10 @@ int main(void)
         //----------------------------------------------------------------------------------
         // TODO: Update your variables here
         //---------------------------------------------------------------------------------
-        
-        camera.target.x = perso.posicao.x - screenWidth /3 + 20;
-        camera.target.y = perso.posicao.y - screenHeight / 3 + 20;
-
-      
-        
-        
-        
-        
-        
-
-        perso.rec.height = frameHeight;
-        perso.rec.width = frameWidth;
-        perso.rec.x = frame * frameWidth;
-        perso.rec.y = posicao * frameHeight;
-        perso.posicao.x = screenWidth / 2 + x;
-        perso.posicao.y = screenHeight /2 + y;
-        perso.hitbox.width = frameWidth / 2;
-        perso.hitbox.height = frameHeight / 2 + 10;
-        perso.hitbox.x = (screenWidth / 2 + frameWidth /4) + x;
-        perso.hitbox.y = (screenHeight / 2 + frameHeight / 4) + y;
-
-        yaux = y;
-        xaux = x;
-
-        if(IsKeyDown(KEY_A) == true) {
-            xaux = xaux - 2;
-            andando = 1;
-            posicao = 1;
-            verificaColisao(&andando, rec_map, perso, posicao);
-            if(andando == 1) x = xaux;
-        }
-        else if(IsKeyDown(KEY_D) == true) {
-            xaux = xaux + 2;
-            andando = 1;
-            posicao = 2;
-            verificaColisao(&andando, rec_map, perso, posicao);
-            if(andando == 1) x = xaux;
-        }
-        else if(IsKeyDown(KEY_W) == true) {
-            yaux = yaux -2;
-            andando = 1;
-            posicao = 3;
-            verificaColisao(&andando, rec_map, perso, posicao);
-            if(andando == 1) y = yaux;
-
-        }
-        else if(IsKeyDown(KEY_S) == true) {
-            yaux = yaux + 2;
-            andando = 1;
-            posicao = 0;
-            verificaColisao(&andando, rec_map, perso, posicao);
-            if(andando == 1) y = yaux;
-        }
-        else {
-            andando = 0;
-            frame = 0;
-        }
-
-        if(andando == 1) {
-            time += GetFrameTime();
-            if(time >= 0.2f) {
-                frame++;
-                time = 0.0f;
-            }
-            frame = frame % 4;
-        }
-        
-        
+            //camera.target.x = perso.posicao.x - (screenWidth /2) /1.5;
+            //camera.target.y = perso.posicao.y - (screenHeight /2) / 1.5;
+        updateCamera(&camera, &perso, screenWidth, screenHeight, mapa);
+        updatePerso(&perso, &x, &y, &frame, &time, frameWidth, frameHeight, &andando, &posicao, screenWidth, screenHeight, rec_map);    
          
         // Draw----------------------------------------
         BeginDrawing();
