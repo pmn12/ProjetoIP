f285a7e2d9f8395563cbf998a7f142ff8bd55250
Ypisdss <114306975+Ypisdss@users.noreply.github.com>
2022-10-12 21:05:37 -0300

    Update CodigoThiago.c

--- CodigoThiago.c
+++ CodigoThiago.c
@@ -29,12 +29,11 @@
 typedef struct{
     Vector2 posicao;
     Rectangle rec;
+    Rectangle recRun;
     Rectangle hitbox;
 } Personagem;
 
-void DesenharPersonagem(Texture2D personagem, Personagem perso, int andando, int posicao) {
-    DrawTextureRec(personagem, perso.rec, perso.posicao, RAYWHITE);
-}
+
 void carregarRetangulos(Rectangle *rec_map,int screenWidth, int screenHeight, Texture2D mapa) {
     
     rec_map[0].x = (screenWidth / 2 - mapa.width /2) + 17;
@@ -98,80 +97,100 @@ void initCamera(Camera2D *camera) {
 }
 
 void updateCamera(Camera2D *camera, Personagem *perso, int screenWidth, int screenHeight, Texture2D mapa) {
-    if(perso -> posicao.x -(screenWidth /2) /1.5 <= (screenWidth /2 - mapa.width /2)) {
+    if(perso -> posicao.x -(screenWidth /2) /2 <= (screenWidth /2 - mapa.width /2)) {
         camera -> target.x = (screenWidth /2 - mapa.width /2);
                      
     }
-    else if(perso -> posicao.x -(screenWidth /2) /1.5 >= mapa.width /2 - ((screenWidth / 2) / 1.5) / 2 ) {
+    else if(perso -> posicao.x -(screenWidth /2) /2 >= mapa.width /2 - ((screenWidth / 2) / 1.5) / 2 ) {
         camera -> target.x = mapa.width /2 - ((screenWidth / 2) / 1.5) /2 ;
                 
     }
     else {
-        camera -> target.x = perso -> posicao.x - (screenWidth /2) /1.5;
+        camera -> target.x = perso -> posicao.x - (screenWidth /2) / 2;
     }
             
-    if(perso -> posicao.y - (screenHeight / 2) / 1.5 <=(screenHeight /2 - mapa.height /2) + ((screenHeight / 2) / 1.5) / 8 ) {
+    if(perso -> posicao.y - (screenHeight / 2) / 2 <=(screenHeight /2 - mapa.height /2) + ((screenHeight / 2) / 1.5) / 8 ) {
         camera -> target.y = (screenHeight /2 - mapa.height /2) + ((screenHeight / 2) / 1.5) / 8;
     }
-    else if(perso -> posicao.y - (screenHeight / 2) / 1.5 >= mapa.height /2 - ((screenHeight / 2) / 1.5) / 2) {
+    else if(perso -> posicao.y - (screenHeight / 2) /2 >= mapa.height /2 - ((screenHeight / 2) / 1.5) / 2) {
         camera -> target.y = mapa.height /2 - ((screenHeight / 2) / 1.5) / 2;
     }
     else {
-        camera -> target.y = perso -> posicao.y - (screenHeight /2) / 1.5;
+        camera -> target.y = perso -> posicao.y - (screenHeight /2) / 2;
     }
 }
 
-void initPerso(Personagem *perso, float frameWidth, float frameHeight) {
-    perso -> rec.height = frameHeight;
-    perso -> rec.width = frameWidth;
-    perso -> hitbox.width = frameWidth / 2;
-    perso -> hitbox.height = frameHeight / 2 + 10;
+void initPerso(Personagem *perso, int screenWidth, int screenHeight, float frameWidthIdle, float frameHeightIdle, float frameWidthRun, float frameHeightRun) {
+    perso -> posicao.x = screenWidth/2;
+    perso -> posicao.y = screenHeight/2;
+    perso -> rec.x = 0;
+    perso -> rec.y = 0;
+    perso -> rec.width = frameWidthIdle;
+    perso -> rec.height = frameHeightIdle;
+    perso -> recRun.x = 0;
+    perso -> recRun.y = 0;
+    perso -> recRun.width = frameWidthRun;
+    perso -> recRun.height = frameHeightRun;
+    perso -> hitbox.width = frameWidthRun / 4;
+    perso -> hitbox.height = frameHeightRun / 4;
 }
 
-void updatePerso(Personagem *perso, int *x, int *y, int *frame, float *time, int frameWidth, int frameHeight, int *andando, int *posicao, int screenWidth, int screenHeight, Rectangle *rec_map) {
-        perso -> rec.x = (*frame) * frameWidth;
-        perso -> rec.y = (*posicao) * frameHeight;
-        perso -> posicao.x = screenWidth / 2 + *x;
-        perso -> posicao.y = screenHeight /2 + *y;
-        perso -> hitbox.x = (screenWidth / 2 + frameWidth /4) + *x;
-        perso -> hitbox.y = (screenHeight / 2 + frameHeight / 4) + *y;
-
-        int yaux = *y;
-        int xaux = *x;
-
+void updatePerso(Personagem *perso, int screenWidth, int screenHeight, float frameWidthIdle, float frameHeightIdle, float frameWidthRun, float frameHeightRun, int *frame, int *frameIdle, float *time, int *x, int *y, Rectangle *rec_map, int *andando, int *i) {
+    int xaux, yaux, posicao = 0;
+        xaux = *x;
+        yaux = *y;
+        perso -> posicao.x = screenWidth/2 + *x;
+        perso -> posicao.y = screenHeight/2 + *y;
+        perso -> hitbox.x = (screenWidth / 2 + frameWidthRun / 4 + frameWidthRun / 8) + *x;
+        perso -> hitbox.y = (screenHeight / 2 + frameHeightRun / 4 + frameWidthRun / 8) + *y;
+        if(*i == 1) {
+            perso -> rec.width = frameWidthIdle;
+            perso -> recRun.width = frameWidthRun;
+        }
+        else {
+            perso -> rec.width = -frameWidthIdle;
+            perso -> recRun.width = -frameWidthRun;
+        }
+        perso -> rec.x = (*frameIdle) * frameWidthIdle;
+        perso -> recRun.x = (*frame) * frameWidthRun;
         if(IsKeyDown(KEY_A) == true) {
             xaux = xaux - 2;
             *andando = 1;
-            *posicao = 1;
-            verificaColisao(andando, rec_map, *perso, *posicao);
+            posicao = 1;
+            *i = -1;
+            verificaColisao(andando, rec_map, *perso, posicao);
+            
             if(*andando == 1) *x = xaux;
         }
         else if(IsKeyDown(KEY_D) == true) {
             xaux = xaux + 2;
             *andando = 1;
-            *posicao = 2;
-            verificaColisao(andando, rec_map, *perso, *posicao);
+            posicao = 2;
+            *i = 1;
+            verificaColisao(andando, rec_map, *perso, posicao);
+            
             if(*andando == 1) *x = xaux;
         }
         else if(IsKeyDown(KEY_W) == true) {
             yaux = yaux -2;
             *andando = 1;
-            *posicao = 3;
-            verificaColisao(andando, rec_map, *perso, *posicao);
+            posicao = 3;
+            verificaColisao(andando, rec_map, *perso, posicao);
+            
             if(*andando == 1) *y = yaux;
 
         }
         else if(IsKeyDown(KEY_S) == true) {
             yaux = yaux + 2;
             *andando = 1;
-            *posicao = 0;
-            verificaColisao(andando, rec_map, *perso, *posicao);
+            posicao = 0;
+            verificaColisao(andando, rec_map, *perso, posicao);
+            
             if(*andando == 1) *y = yaux;
         }
-        else {
-            *andando = 0;
-            *frame = 0;
-        }
+        else *andando = 0;
+
+        
 
         if(*andando == 1) {
             *time += GetFrameTime();
@@ -179,8 +198,28 @@ void updatePerso(Personagem *perso, int *x, int *y, int *frame, float *time, int
                 (*frame)++;
                 *time = 0.0f;
             }
-            *frame = *frame % 4;
+            *frame = *frame % 8;
+            *frameIdle = 0;
         }
+        else {
+            *frame = 0;
+
+            *time += GetFrameTime();
+            if(*time >= 0.2f) {
+                (*frameIdle)++;
+                *time = 0.0f;
+            }
+            *frameIdle = *frameIdle % 10;
+         }   
+}
+
+void desenharPerso(Texture2D persoIdle, Texture2D persoRun, Personagem perso, int andando) {
+    if(andando == 0) {
+        DrawTextureRec(persoIdle, perso.rec, perso.posicao, RAYWHITE);
+    }
+    else {
+        DrawTextureRec(persoRun, perso.recRun, perso.posicao, RAYWHITE);
+    }
 }
 
 
@@ -203,23 +242,29 @@ int main(void)
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
     // Textures:
-    int x = 0, y = 0, andando = 0, posicao = 0;
-    Personagem perso;
+    int x = 0, y = 0, andando = 0, i = 1;
     
     Camera2D camera;
     initCamera(&camera);
      
     Texture2D mapa = LoadTexture("./Assets/MapaDefinitivo.png");
-    Texture2D personagem = LoadTexture("./Assets/perso.png");
-    float frameWidth = personagem.width / 4.0;
-    float frameHeight = personagem.height / 4.0;
+    Texture2D persoIdle = LoadTexture("./Assets/Idle.png");
+    Texture2D persoRun = LoadTexture("./Assets/Run.png");
+    float frameWidthIdle = persoIdle.width / 10.0;
+    float frameHeightIdle = persoIdle.height;
+    float frameWidthRun = persoRun.width / 8.0;
+    float frameHeightRun = persoRun.height;
+    int frameIdle = 0;
     int frame = 0;
     float time = 0.0f;
 
-    initPerso(&perso, frameWidth, frameHeight);
+    
 
     Rectangle *rec_map = (Rectangle *) malloc(10 * sizeof(Rectangle));
     carregarRetangulos(rec_map, screenWidth, screenHeight, mapa);
+
+    Personagem perso;
+    initPerso(&perso, screenWidth, screenHeight, frameWidthIdle, frameHeightIdle, frameWidthRun, frameHeightRun);
     
     
     // Main game loop
@@ -231,16 +276,25 @@ int main(void)
         //---------------------------------------------------------------------------------
             //camera.target.x = perso.posicao.x - (screenWidth /2) /1.5;
             //camera.target.y = perso.posicao.y - (screenHeight /2) / 1.5;
-        updateCamera(&camera, &perso, screenWidth, screenHeight, mapa);
-        updatePerso(&perso, &x, &y, &frame, &time, frameWidth, frameHeight, &andando, &posicao, screenWidth, screenHeight, rec_map);    
-         
+        //updateCamera(&camera, &perso, screenWidth, screenHeight, mapa);
+        //updatePerso(&perso, &x, &y, &frame, &time, frameWidth, frameHeight, &andando, &posicao, screenWidth, screenHeight, rec_map);
+       updatePerso(&perso, screenWidth, screenHeight, frameWidthIdle, frameHeightIdle, frameWidthRun, frameHeightRun, &frame, &frameIdle, &time, &x, &y, rec_map, &andando, &i);
+       updateCamera(&camera, &perso, screenWidth, screenHeight, mapa);
+       if(i == 1) {
+            perso.rec.width = frameWidthIdle;
+            perso.recRun.width = frameWidthRun;
+        }
+        else {
+            perso.rec.width = -frameWidthIdle;
+            perso.recRun.width = -frameWidthRun;
+        }
         // Draw----------------------------------------
         BeginDrawing();
         ClearBackground(BLACK);
         BeginMode2D(camera);
-            DrawTexture(mapa, screenWidth / 2 - mapa.width /2 ,screenHeight / 2 - mapa.height/2 , RAYWHITE);
-            DesenharPersonagem(personagem, perso, andando, posicao);
-
+           DrawTexture(mapa, screenWidth / 2 - mapa.width /2 ,screenHeight / 2 - mapa.height/2 , RAYWHITE);
+            desenharPerso(persoIdle, persoRun, perso, andando);
+            
             
           
             
